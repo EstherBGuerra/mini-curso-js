4ebda1439fa992573fd1d9d723d276583c4ad4b1
Esther <estherguerra23@gmail.com>
2020-07-26 18:49:28 -0300

    ultimo commit da penultima aula

--- js/components/WrapperCharacters/index.js
+++ js/components/WrapperCharacters/index.js
@@ -1,8 +1,6 @@
 const pathCupcake = 'imagens/cupcake.png'
 const cupcake = Character `
     width: 30%;
-    margin-left: -1.5rem;
-    margin-right: -1.5rem;
     ${pathCupcake}
 `
 
@@ -12,6 +10,6 @@ const WrapperCharacters = (css, children) => (`
 
 const wrapperCharacters = WrapperCharacters `
     display: flex;
-    justify-content: space-around;
-    ${cupcake + cupcake + cupcake}
+    justify-content: space-evenly;
+    ${cupcake + cupcake + cupcake} 
 `
